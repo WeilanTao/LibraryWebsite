feb7d681f2069a5cc469d70fc08260b8f9377d0e
WeilanTao <twlemily@hotmail.com>
2022-04-08 12:54:49 -0400

    the book model

--- Library/books/models.py
+++ Library/books/models.py
@@ -1,3 +1,19 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    author_name = models.CharField(max_length = 255)
+    author_tag = models.CharField(max_length = 255)
+
+class Book(models.Model):
+    book_name = models.CharField(max_length = 255)
+    book_tag = models.CharField(max_length = 255)
+
+    # to delete the book when the author is deleted-- cascading
+    author_tag = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+class Chapter(models.Model):
+    chapter_name: models.CharField(max_length = 255)
+    chapter_content: models.TextField()
+    book_tag = models.ForeignKey(Book, on_delete=models.CASCADE )
