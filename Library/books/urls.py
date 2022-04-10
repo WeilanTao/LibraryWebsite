52a84e4bcf1c72e15cf4bfcf154a95d2e5cde3e0
WeilanTao <twlemily@hotmail.com>
2022-04-10 12:53:56 -0400

    first step...get data from database

--- Library/books/urls.py
+++ Library/books/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 # root url: books/
-# books/<author_tag>
-# books/<book_tag>
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.get_authors, name ="get_authors"),
+    path("booklist", views.get_books, name="get_books"),
+]
--- Library/books/views.py
+++ Library/books/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from books.models import Author, Book, Chapter
 import sys 
 import os
 
@@ -10,3 +10,17 @@ def index(request):
     return HttpResponse("this is: ", sys.path.append(os.path.dirname(os.path.abspath('.'))))
 
 
+def get_authors(request):
+    authors = Author.objects.values()
+
+    return HttpResponse(authors)
+
+def get_books(request):
+    books = Book.objects.values()
+
+    return HttpResponse(books)       
+
+def get_chapters(request):
+    chapters = Chapter.objects.values()
+
+    return HttpResponse(chapters)
