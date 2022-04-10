24d0550fe7fed5b60a87bc05b9ea340666c65ddd
WeilanTao <twlemily@hotmail.com>
2022-04-10 14:35:58 -0400

    url dispatcher set up; Todo: make the dinamic url and redirect for various books

--- Library/books/urls.py
+++ Library/books/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 # root url: books/
+app_name = 'books'
 urlpatterns = [
     path("", views.get_authors, name ="get_authors"),
     path("booklist", views.get_books, name="get_books"),
