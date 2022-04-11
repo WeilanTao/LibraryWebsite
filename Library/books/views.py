ae977c064248f201d4de43f7d9c4ec446922fd54
WeilanTao <twlemily@hotmail.com>
2022-04-11 13:05:54 -0400

    minor naming issue

--- Library/books/views.py
+++ Library/books/views.py
@@ -14,45 +14,45 @@ def get_authors(request):
 
     return render(request, 'authorslist.html', context = data)
 
-def get_books(request, auth_tag):
-    books = Book.objects.filter(author_tag = auth_tag)
+def get_books(request, author_tag):
+    books = Book.objects.filter(author_tag = author_tag)
 
-    author_name = get_author_name(auth_tag)
+    author_name = get_author_name(author_tag)
 
     data={
         "books":books,
-        "auth_tag" : auth_tag,
-        "auth_name" : author_name
+        "author_tag" : author_tag,
+        "author_name" : author_name
     }
 
     return render(request, 'booklist.html', context=data)       
 
-def get_chapters(request, auth_tag, book_tag):
+def get_chapters(request, author_tag, book_tag):
     chapters = Chapter.objects.filter(book_tag = book_tag).defer('chapter_content').order_by('chapter_index')
 
     book_name = get_book_name(book_tag)
 
     data = {
         "chapters":chapters,
-        'author':auth_tag,
+        "author_tag":author_tag,
         "book_name":book_name
     }
 
     return render(request, 'chapterlist.html', context=data)
 
-def get_chapter_content(request, auth_tag, book_tag, chapter_id):
+def get_chapter_content(request, author_tag, book_tag, chapter_id):
 
     chapter = Chapter.objects.filter(id = chapter_id).first()
     book_name = get_book_name(book_tag)
     data ={
         "chapter":chapter,
-        "author": auth_tag,
+        "author_tag": author_tag,
         "book_name":book_name
     }
 
     return render(request, 'chaptercontent.html', context=data)
 
-def get_next_previous(request, auth_tag, book_tag, chapter_id, next_previous):
+def get_next_previous(request, author_tag, book_tag, chapter_id, next_previous):
     chapters = Chapter.objects.filter(book_tag = book_tag).defer('chapter_content').order_by('chapter_index')
 
     # print(type(next_previous), type(0))
@@ -69,7 +69,7 @@ def get_next_previous(request, auth_tag, book_tag, chapter_id, next_previous):
         if isFound == 1:
             data = {
                 "chapter":chapter,
-                "author": auth_tag,
+                "author_tag": author_tag,
                 "book_name":book_name
             }
             return render(request, 'chaptercontent.html', context=data)
@@ -78,30 +78,10 @@ def get_next_previous(request, auth_tag, book_tag, chapter_id, next_previous):
             isFound = 1
 
     return HttpResponse("hELLO")
+  
 
-
-
-def get_previous_chapter(request, auth_tag, book_tag, chapter_id):
-    chapters = list(Chapter.objects.filter(book_tag = book_tag).defer('chapter_content').order_by('chapter_index').reverse())
-
-    isFound = 0
- 
-
-    for chapter in chapters:        
-        if isFound == 1:
-            data = {
-                "chapter":chapter,
-                "author": auth_tag
-            }
-            return render(request, 'chaptercontent.html', context=data)
-        if str(chapter.id) == chapter_id:
-            print("Found")
-            isFound = 1
-
-    return HttpResponse("hELLO")    
-
-def get_author_name (auth_tag):
-    res_name = Author.objects.filter(author_tag = auth_tag).only('author_name').first()
+def get_author_name (author_tag):
+    res_name = Author.objects.filter(author_tag = author_tag).only('author_name').first()
     return res_name.author_name
 
 
