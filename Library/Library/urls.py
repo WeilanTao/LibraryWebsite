27dcdbe99bf845fa7ddfe7a932144331e2e0bb24
WeilanTao <twlemily@hotmail.com>
2022-04-11 06:28:09 -0400

    some minor url issue

--- Library/Library/urls.py
+++ Library/Library/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls), 
-    path("books/", include("books.urls",namespace="books"))
+    path("", include("books.urls",namespace="books"))
     ]
--- Library/books/urls.py
+++ Library/books/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'books'
 urlpatterns = [
     path("", views.get_authors, name ="get_authors"),
-    path("book/<auth_tag>/", views.get_books, name="get_books"),
-    path("book/<auth_tag>/<book_tag>/", views.get_chapters, name="get_chapters"),
-    path("book/<auth_tag>/<book_tag>/<chapter_id>", views.get_chapter_content, name ="get_content")
+    path("<auth_tag>/", views.get_books, name="get_books"),
+    path("<auth_tag>/<book_tag>/", views.get_chapters, name="get_chapters"),
+    path("<auth_tag>/<book_tag>/<chapter_id>", views.get_chapter_content, name ="get_content")
 ]
