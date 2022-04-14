90b40f1a63b82f3a6e8bf2f8e81851ff2a76fcb7
WeilanTao <twlemily@hotmail.com>
2022-04-14 09:02:10 -0400

    this is how to use bootstrap modal to implement the pop up window for django template

--- Library/static/books/js/books.js
+++ Library/static/books/js/books.js
@@ -1,3 +1,15 @@
 function Add_to_List(book_name) {
-  console.log(book_name);
+  // console.log(book_name);
+
+  $("#exampleModal").on("show.bs.modal", function (event) {
+    // var button = event.relatedTarget;
+    // console.log("hihihi");
+    // // Extract info from data-bs-* attributes
+
+    // var recipient = button.getAttribute("data-bs-whatever");
+    // Update the modal's content.
+    var modalTitle = exampleModal.querySelector(".modal-title");
+    modalTitle.textContent = book_name;
+  });
+  $("#exampleModal").modal("show");
 }
