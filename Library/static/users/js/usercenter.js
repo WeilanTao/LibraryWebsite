79586137403ffa0d96477ee36d0a3d041eb54d8c
WeilanTao <twlemily@hotmail.com>
2022-04-16 06:18:40 -0400

    ajax async

--- Library/static/users/js/usercenter.js
+++ Library/static/users/js/usercenter.js
@@ -10,40 +10,43 @@ function getBooks(booklist_id) {
       for (let b in data.books) {
         const bookobj = data.books[b];
 
-        const book = getBookInfo(bookobj["book_tag"]);
-        console.log("get...", book);
+        getBookInfo(bookobj["book_tag"]).then((response) => {
+          // console.log("get...", response.book[0]);
 
-        const book_li = document.createElement("li");
+          const book = response.book[0];
+          // console.log("hihihi", book.book_name);
+          const book_li = document.createElement("li");
 
-        book_li.innerHTML =
-          book_li.innerHTML +
-          "<button type='button' onclick=\"goToBook('" +
-          book.author_tag +
-          ":" +
-          book.book_tag +
-          "')\" >" +
-          book.book_name +
-          "</button> <button type = 'button' onclick =\"deleteBookFromList('" +
-          book.book_tag +
-          "')\" >  Delete Book</button>";
+          book_li.innerHTML =
+            book_li.innerHTML +
+            "<button type='button' onclick=\"goToBook('" +
+            book.author_tag +
+            ":" +
+            book.book_tag +
+            "')\" >" +
+            book.book_name +
+            "</button> <button type = 'button' onclick =\"deleteBookFromList('" +
+            book.book_tag +
+            "')\" >  Delete Book</button>";
 
-        document.getElementById("books_li").appendChild(book_li);
+          document.getElementById("books_li").appendChild(book_li);
+        });
       }
     }
   );
 }
 
 function getBookInfo(book_tag) {
-  console.log(book_tag);
-  $.getJSON(
-    "/get/book/infor/ma/tion/",
-    { book_tag: book_tag },
-    function (data) {
-      console.log("HIHIHI", data.book[0]);
-      return data.book[0];
-    }
-  );
+  return $.ajax({
+    url: "/get/book/infor/ma/tion/",
+    data: { book_tag: book_tag },
+    async: false,
+    done: function (data) {
+      return data;
+    },
+  });
 }
+
 function goToBook(inputstr) {
   const author_tag = inputstr.split(":")[0];
   const book_tag = inputstr.split(":")[1];
